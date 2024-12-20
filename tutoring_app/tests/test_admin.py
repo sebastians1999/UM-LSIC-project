ed04728dbb06dc8342f753dca7e5038ad107692f
KJMvanderHorst <115348690+KJMvanderHorst@users.noreply.github.com>
2024-12-20 17:18:33 +0100

    fixed imports

--- tutoring_app/tests/test_admin.py
+++ tutoring_app/tests/test_admin.py
@@ -2,9 +2,9 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.main import app 
-from app.database.database import Base, get_db
-from app.routers.authentication import create_user_in_db, generate_admin_token
+from ..app.main import app 
+from ..app.database.database import Base, get_db
+from ..app.routers.authentication import create_user_in_db, generate_admin_token
 
 # Create a test database
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
@@ -34,8 +34,6 @@ def test_db():
     yield db
     db.close()
 
-#now i want to make test that uses generate_admin_token to automatically create an admin user in the test database
-#and then use the token to access the admin-only endpoint /admin/users and get a list of all users and verify that there is onely 1 user
 def test_admin_user_access(test_db):
     # Generate an admin token, this also automatically creates an admin user in the test database
     admin_token = generate_admin_token(test_db)
@@ -51,4 +49,3 @@ def test_admin_user_access(test_db):
     users = response.json()
     assert len(users) == 1
     assert users[0]["username"] == "admin"
-
